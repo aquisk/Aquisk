294d8cca39e15e18d2cc13e7964c1e45a13c53f5
ax-6 <ax-6@outlook.com>
2023-12-03 21:01:28 +0800

    Some files in the AqDebug library have been updated.
    The AqDebug library has not been developed yet and is for reference only.

--- aq_debug/aq_debug.cc
+++ aq_debug/aq_debug.cc
@@ -1,10 +1,14 @@
 #include "aq_debug/aq_debug.h"
-void AqDebug::AqErrorHandling(AqDebug::AqDebugData ErrorReportContent){
-//AqDebug::AqDebugOutput();
+#include "aq_debug/aq_debug_output.h"
+void AqDebug::AqDebugReport::AqErrorHandling(AqDebug::AqDebugData ErrorReportContent){
+std::string OutputMessage = ErrorReportContent.Code + ": " + ErrorReportContent.Message;
+AqDebug::AqDebugOutput(OutputMessage);
 }
-void AqDebug::AqWarningHandling(AqDebug::AqDebugData WarningReportContent){
-
+void AqDebug::AqDebugReport::AqWarningHandling(AqDebug::AqDebugData WarningReportContent){
+std::string OutputMessage = WarningReportContent.Code + ": " + WarningReportContent.Message;
+AqDebug::AqDebugOutput(OutputMessage);
 }
-void AqDebug::AqInfoHandling(AqDebug::AqDebugData InfoReportContent){
-
+void AqDebug::AqDebugReport::AqInfoHandling(AqDebug::AqDebugData InfoReportContent){
+std::string OutputMessage = InfoReportContent.Code + ": " + InfoReportContent.Message;
+AqDebug::AqDebugOutput(OutputMessage);
 }
--- aq_debug/aq_debug.h
+++ aq_debug/aq_debug.h
@@ -3,25 +3,37 @@
 #include <iostream>
 #include "aq_debug/aq_debug_output.h"
 namespace AqDebug{
-    struct AqDebugData{
-        std::string Code;
-        std::string Message;
+    class AqDebugData{
+        public:
+            std::string Code;
+            std::string Message;
     };
-    extern void AqErrorHandling(AqDebug::AqDebugData ErrorReportContent);
-    extern void AqWarningHandling(AqDebug::AqDebugData WarningReportContent);
-    extern void AqInfoHandling(AqDebug::AqDebugData InfoReportContent);
     class AqDebugReport{
         public:
-            void ErrorReport(AqDebug::AqDebugData ErrorReportContent){
-                AqDebug::AqErrorHandling(ErrorReportContent);
+            std::string Code;
+            std::string Message;
+            void ErrorReport(){
+                AqDebug::AqDebugData ErrorReportContent;
+                ErrorReportContent.Code = Code;
+                ErrorReportContent.Message = Message;
+                AqErrorHandling(ErrorReportContent);
             }
-            void WarningReport(AqDebug::AqDebugData WarningReportContent){
-                AqDebug::AqWarningHandling(WarningReportContent);
+            void WarningReport(){
+                AqDebug::AqDebugData WarningReportContent;
+                WarningReportContent.Code = Code;
+                WarningReportContent.Message = Message;
+                AqWarningHandling(WarningReportContent);
             }
-            void InfoReport(AqDebug::AqDebugData InfoReportContent){
-                AqDebug::AqInfoHandling(InfoReportContent);
+            void InfoReport(){
+                AqDebug::AqDebugData InfoReportContent;
+                InfoReportContent.Code = Code;
+                InfoReportContent.Message = Message;
+                AqInfoHandling(InfoReportContent);
             }
+        private:
+            void AqErrorHandling(AqDebug::AqDebugData ErrorReportContent);
+            void AqWarningHandling(AqDebug::AqDebugData WarningReportContent);
+            void AqInfoHandling(AqDebug::AqDebugData InfoReportContent);
     };
-    
 }
 #endif
