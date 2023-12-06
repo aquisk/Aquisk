c493de51619a0e71fccddf8b2fd85f21334753b2
ax-6 <ax-6@outlook.com>
2023-12-06 21:52:20 +0800

    Updated part of the Aquisk Debug library, test version, for reference only.

--- aq_debug/aq_debug.cc
+++ aq_debug/aq_debug.cc
@@ -1,17 +1,18 @@
 // (c) Copyright 2023 Aquisk (AQ Project) Authors
 // Use of this source code is governed by a Aquisk License that can be found in the LICENSE file.
+
 // This file defines the Aquisk debug library information handling function.
 #include "aq_debug/aq_debug.h"
-// Process the debug content and output it through the interface.
-void AqDebug::AqDebugReport::AqErrorHandling(AqDebug::AqDebugReport::AqDebugData ErrorReportContent){
-    std::string OutputMessage = ErrorReportContent.Code + ": " + ErrorReportContent.Message;
+// Process the debug data and output it through the interface.
+void AqDebug::AqDebugReport::ErrorHandling(AqDebug::AqDebugReport::AqDebugData ErrorReportData){
+    std::string OutputMessage = ErrorReportData.Code + ": " + ErrorReportData.Message;
     AqDebug::AqDebugOutput(OutputMessage);
 }
-void AqDebug::AqDebugReport::AqWarningHandling(AqDebug::AqDebugReport::AqDebugData WarningReportContent){
-    std::string OutputMessage = WarningReportContent.Code + ": " + WarningReportContent.Message;
+void AqDebug::AqDebugReport::WarningHandling(AqDebug::AqDebugReport::AqDebugData WarningReportData){
+    std::string OutputMessage = WarningReportData.Code + ": " + WarningReportData.Message;
     AqDebug::AqDebugOutput(OutputMessage);
 }
-void AqDebug::AqDebugReport::AqInfoHandling(AqDebug::AqDebugReport::AqDebugData InfoReportContent){
-    std::string OutputMessage = InfoReportContent.Code + ": " + InfoReportContent.Message;
+void AqDebug::AqDebugReport::InfoHandling(AqDebug::AqDebugReport::AqDebugData InfoReportData){
+    std::string OutputMessage = InfoReportData.Code + ": " + InfoReportData.Message;
     AqDebug::AqDebugOutput(OutputMessage);
 }
--- aq_debug/aq_debug.h
+++ aq_debug/aq_debug.h
@@ -1,5 +1,6 @@
 // (c) Copyright 2023 Aquisk (AQ Project) Authors
 // Use of this source code is governed by a Aquisk License that can be found in the LICENSE file.
+
 // This file defines the interface of the Aquisk Debug library.
 #ifndef aq_debug
 #define aq_debug
@@ -9,39 +10,29 @@ namespace AqDebug{
     // Interface to Aquisk debug library.
     class AqDebugReport{
         public:
-            // Parameters for the Aquisk debugging library interface.
-            std::string Code;
-            std::string Message;
-            // Aquisk debug library internal classes.
+            // Aquisk debug library parameter class.
             class AqDebugData{
                 public:
                     std::string Code;
                     std::string Message;
             };
-            // Aquisk debug library call method.
-            void AqErrorReport(){
-                AqDebugData ErrorReportContent;
-                ErrorReportContent.Code = Code;
-                ErrorReportContent.Message = Message;
-                AqErrorHandling(ErrorReportContent);
+            // Parameters for the Aquisk debugging library interface.
+            AqDebugData Data;
+            // Aquisk debugging library call interface.
+            void ErrorReport(){
+                ErrorHandling(Data);
             }
-            void AqWarningReport(){
-                AqDebugData WarningReportContent;
-                WarningReportContent.Code = Code;
-                WarningReportContent.Message = Message;
-                AqWarningHandling(WarningReportContent);
+            void WarningReport(){
+                WarningHandling(Data);
             }
-            void AqInfoReport(){
-                AqDebugData InfoReportContent;
-                InfoReportContent.Code = Code;
-                InfoReportContent.Message = Message;
-                AqInfoHandling(InfoReportContent);
+            void InfoReport(){
+                InfoHandling(Data);
             }
         private:
-            // Aquisk debug library content handling function.
-            void AqErrorHandling(AqDebugData ErrorReportContent);
-            void AqWarningHandling(AqDebugData WarningReportContent);
-            void AqInfoHandling(AqDebugData InfoReportContent);
+            // Aquisk debug library data handling function.
+            void ErrorHandling(AqDebugData ErrorReportData);
+            void WarningHandling(AqDebugData WarningReportData);
+            void InfoHandling(AqDebugData InfoReportData);
     };
 }
 #endif
