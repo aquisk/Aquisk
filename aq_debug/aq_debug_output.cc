48a45619c0b12acf82bac54b6ad594c98bc7ea4f
AX网 <ax-6@outlook.com>
2024-01-27 11:50:29 +0800

    Waiting for redevelopment.

--- aq_debug/aq_debug_output.cc
+++
@@ -1,8 +0,0 @@
-// (c) Copyright 2023 Aquisk (AQ Project) Authors
-// Use of this source code is governed by a Aquisk License that can be found in the LICENSE file.
-// This file defines the output functions of the Aquisk Debug library.
-#include "aq_debug/aq_debug_output.h"
-void AqDebug::AqDebugOutput(std::string OutputMessage){
-    // The content of this function is for reference only and will be developed according to specific projects.
-    std::cout << OutputMessage << std::endl;
-}
